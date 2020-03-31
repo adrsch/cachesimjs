85cb576c3a245848fba144c3aa7b32bed065b05b
adrsch <35346279+adrsch@users.noreply.github.com>
2020-03-31 14:53:26 -0700

    Add MemorySystem class for processing instructions and running them on the simulated caches

--- cachesim.js
+++ cachesim.js
@@ -159,7 +159,7 @@ class Cache {
     }
 
     split( address ) {
-        var unsignedAddress = parseInt( addressHex, 16 ) >>> 0;
+        var unsignedAddress = parseInt( address, 16 ) >>> 0;
         var tag = this.getTag( unsignedAddress );
         var index = this.getIndex( unsignedAddress );
         var offset = this.getOffset( unsignedAddress );
@@ -337,62 +337,46 @@ class WriteBackLRU extends LRUCache {
 }
 
 class MemorySystem {
-}
-// Run a single instruction
-function runInstruction( type, address, dataCache, instructionCache ) {
-    if ( type == 'lw' || type == 0 ) {
-        dataCache.readAddress( address );
+    constructor( dataCache, instructionCache=null  ) {
+        this.dataCache = dataCache;
+        this.instructionCache = instructionCache;
     }
-    else if ( type == 'write' || type == 1 ) {
-        dataCache.writeAddress( address );
+
+    runInstruction( type, address ) {
+        if ( type == 'lw' || type == 'data read' || type == 0 ) {
+            this.dataCache.readAddress( address );
+        }
+        else if ( type == 'sw' || type == 'data write' || type == 1 ) {
+            this.dataCache.writeAddress( address );
+        }
+        else if ( this.instructionCache && ( type == 'instruction read' || type == 2 ) ) {
+            this.instructionCache.readAddress( address );
+        }
+        else { throw 'Invalid instruction'; }
     }
-    else if ( type == 'readi' || type == 2 ) {
-        instructionCache.readAddress( address );
+    
+    // Read trace of memory references at a file
+    readTrace( trace ) {
+        traceEntries = trace.toString().split("\n");
+        traceEntries.forEach( function( entry ) {
+            reference = entry.split( " " );
+            this.runInstruction( reference[0], reference[1] ); 
+        });
     }
-}
 
-// Read trace of memory references to a data and instruction cache
-function read( trace, dataCache, instructionCache ) {
-    traceEntries = trace.toString().split("\n");
-    traceEntries.forEach( function( entry ) {
-        reference = entry.split( " " );
-        runInstruction( reference[0], reference[1], dataCache, instructionCache ); 
-    });
-}
+    randomType() {
+        if ( this.instructionCache ) {
+            return Math.floor( Math.random() * 3 );
+        }
+        else {
+            return Math.floor( Math.random() * 2 );
+        }
+    }
+    randomAddress() {
+        var randomIntArray = new Uint32Array( 1 );
+        window.crypto.getRandomValues( randomIntArray );
+        return randomIntArray[0].toString(16);
+    }
 
- 
-
-// Generate an instruction type as an int: 0, 1, or 2
-function generateType() {
-    return Math.floor( Math.random() * 4 ) + 1;
-}
-
-// Generate an address
-function generateAddress() {
-    var randomIntArray = new Uint32Array( 1 );
-    window.crypto.getRandomValues( randomIntArray );
-    return toHex( randomIntArray[0] );
-}
-
-function amat2( l1d, l1i, h, m ) {
-    return h + ( ( ( l1d.getMisses() + l1i.getMisses() ) / ( l1d.getMisses() + l1i.getMisses() + l1d.getHits() + l1i.getHits() ) ) * m );
-}
-
-function amatL2( l1d, l1i, l2, l1h, l2h, l2m ) {
-    return l1h + ( ( ( l1d.getMisses() + l1i.getMisses() ) / ( l1d.getMisses() + l1i.getMisses() + l1d.getHits() + l1i.getHits() ) ) * ( l2h + ( l2.getMisses() / ( l2.getHits() + l2.getMisses() ) ) * l2m ) );
-}
-
-
-
-function csv( print, l1d, l1i, h, m ) {
-    print( l1d.assoc + "," + ( l1i.getHits() + l1i.getMisses() ) + "," + l1i.getMisses() + "," + ( l1d.getHits() + l1d.getMisses() ) + "," + l1d.getMisses() + "," + ( l1i.getHits() / ( l1i.getHits() + l1i.getMisses() ) ) + "," + ( l1d.getHits() / ( l1d.getHits() + l1d.getMisses() ) ) + "," + amat2( l1d, l1i, h, m ) );
-}
-
-function csv3( print, l1d, l1i, l2, l1h, l2h, l2m, p ) {
-    print( p + "," + ( l1i.getHits() + l1i.getMisses() ) + "," + l1i.getMisses() + "," + ( l1d.getHits() + l1d.getMisses() ) + "," + l1d.getMisses() + "," + ( l1i.getHits() / ( l1i.getHits() + l1i.getMisses() ) ) + "," + ( l1d.getHits() / ( l1d.getHits() + l1d.getMisses() ) ) + "," + ( l2.getHits() + l2.getMisses() ) + "," + l2.getMisses() + "," + ( l2.getHits() / ( l2.getHits() + l2.getMisses() ) ) + "," + amatL2( l1d, l1i, l2, l1h, l2h, l2m ) );
-}
-
-function csv2( print, l1d, l1i, l2, l1h, l2h, l2m ) {
-    print( l2.assoc + "," + ( l1i.getHits() + l1i.getMisses() ) + "," + l1i.getMisses() + "," + ( l1d.getHits() + l1d.getMisses() ) + "," + l1d.getMisses() + "," + ( l1i.getHits() / ( l1i.getHits() + l1i.getMisses() ) ) + "," + ( l1d.getHits() / ( l1d.getHits() + l1d.getMisses() ) ) + "," + ( l2.getHits() + l2.getMisses() ) + "," + l2.getMisses() + "," + ( l2.getHits() / ( l2.getHits() + l2.getMisses() ) ) + "," + amatL2( l1d, l1i, l2, l1h, l2h, l2m ) );
 }
 
